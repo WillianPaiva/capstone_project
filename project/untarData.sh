27dd5059103f143e0b12e985004412ea4e9b1d86
willian ver valen <vervalenpaiva@gmail.com>
2017-10-31 14:15:19 +0100

    update gitignore

---
+++ project/untarData.sh
@@ -0,0 +1,22 @@
+mkdir decompressed_dataset
+
+mkdir decompressed_dataset/a
+tar zxf dataset/muct-a-jpg-v1.tar.gz -C decompressed_dataset/a
+
+mkdir decompressed_dataset/b
+tar zxf dataset/muct-b-jpg-v1.tar.gz -C decompressed_dataset/b
+
+
+mkdir decompressed_dataset/c
+tar zxf dataset/muct-c-jpg-v1.tar.gz -C decompressed_dataset/c
+
+
+mkdir decompressed_dataset/d
+tar zxf dataset/muct-d-jpg-v1.tar.gz -C decompressed_dataset/d
+
+
+mkdir decompressed_dataset/e
+tar zxf dataset/muct-e-jpg-v1.tar.gz -C decompressed_dataset/e
+
+
+tar zxf dataset/muct-landmarks-v1.tar.gz -C decompressed_dataset
